7d0552b1c8b8a9e88f7c55702930db739a8d56f1
Ashley Riehl and Laura Krouse <Guest@epicodus-95.local>
2016-06-29 19:55:06 -0700

    added js buisness logic to allow users to submit addresses

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,48 @@
 function Contact(first,last) {
   this.firstName = first;
   this.lastName = last;
+  this.addresses = [];
 }
-
+function Address(street, city,state, country) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+  this.country = country;
+}
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state + ", " + this.country;
+}
 
 //user interface logic
+
+
 $(document).ready(function() {
+  function resetFeilds() {
+ $("input#new-first-name").val("");
+ $("input#new-last-name").val("");
+ $("input.new-street").val("");
+$("input.new-city").val("");
+$("input.new-state").val("");
+$("input.new-country").val("");
+}
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                 '<div class="form-group">' + '<label for="new-street">Street</label>' +
+                                   '<input type="text" class="form-control new-street">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-city">City</label>' +
+                                   '<input type="text" class="form-control new-city">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-state">State</label>' +
+                                   '<input type="text" class="form-control new-state">' +
+                                 '</div>' +
+                               '</div>');
+  });
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
 
@@ -18,6 +52,18 @@ $(document).ready(function() {
 
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
+
+    $(".new-address").each(function() {
+      var inputtedStreet = $(this).find("input.new-street").val();
+      var inputtedCity = $(this).find("input.new-city").val();
+      var inputtedState = $(this).find("input.new-state").val();
+      var inputtedCountry = $(this).find("input.new-country").val();
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState, inputtedCountry);
+      newContact.addresses.push(newAddress);
+    });
+
+
+
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
      $(".contact").last().click(function() {
@@ -25,9 +71,11 @@ $(document).ready(function() {
        $("#show-contact h2").text(newContact.firstName);
        $(".first-name").text(newContact.firstName);
        $(".last-name").text(newContact.lastName);
+       $("ul#addresses").text("");
+       newContact.addresses.forEach(function(address) {
+         $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
+       });
      });
-
-     $("input#new-first-name").val("");
-     $("input#new-last-name").val("");
+  resetFeilds()
   });
 });
